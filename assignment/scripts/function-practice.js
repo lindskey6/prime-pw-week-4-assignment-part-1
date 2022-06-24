8c5846d9e6080a199a24bbd4ead48a36d22df611
Linssi Colin Ruiz <linssi101@yahoo.com>
2022-06-24 17:12:19 -0500

    I think im getting the hang of this. Need to study a bit more

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -5,46 +5,56 @@ console.log('***** Function Practice *****')
 // to test it and display the result
 
 // 1. Function to return 'Hello World!'
-function hello() {
+function hello(){
   return 'Hello World!';
 }
 // Call the function to test
 console.log('Test - should say "Hello World!"', hello());
 
+console.log(hello());
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
-function helloName( name ) {
-  return;
+function helloName(name){
+  return 'linssi';
 }
+
 // Remember to call the function to test
+console.log(helloName());
+
 
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
+function addNumbers(firstNumber){
+  
   // return firstNumber + secondNumber;
+  return (4 + 6);
 }
 
+console.log(addNumbers());
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
-
+function multiplyThree(){
+return (3 * 4 * 1);
 }
-
+ console.log(multiplyThree());
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
-function isPositive( number ) {
-  if ( number > 0 ){
+function isPositive(number){
+  if (number){
     return;
   }
     return;
 }
+
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
+
 console.log( 'isPositive - should say true', isPositive(3) );
 console.log( 'isPositive - should say false', isPositive(0) );
 console.log( 'isPositive - should say false', isPositive(-3) );
+
 
 
 // 6. Function to return the _last_ item in an array. If the 
@@ -87,3 +97,12 @@ function sumAll( ) {
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
 //     here in a comment, write the function, and test it!
+
+
+
+
+
+
+
+
+
